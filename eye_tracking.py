f7740e876eee8760e98cff6eb37816c07a5cfa33
Gregory Specchi <gregory.specchi@unikey.it>
2023-09-26 18:22:39 +0200

    aggiuno cerchio nelle pupille

--- eye_tracking.py
+++ eye_tracking.py
@@ -7,24 +7,30 @@ cap = cv2.VideoCapture(0)
 
 
 def eye_tracker(img):
-    results = model(source=img, show=False, conf=0.7, save=False)
-    return results[0]
+    res = model(source=img, show=False, conf=0.7, save=False)
+    res = res[0]
+    eye_boxes = {}
+    for i in range(len(res.boxes)):
+        box = res.boxes[i]
+        tensor = box.xyxy
+        values_list = tensor[0].tolist()
+        if len(values_list) != 0:
+            eye_boxes = {'x1': int(values_list[0]),
+                         'y1': int(values_list[1]),
+                         'x2': int(values_list[2]),
+                         'y2': int(values_list[3]),
+                         'prob': round(box.conf[0].item(), 2)}
+    return eye_boxes
 
 
 while True:
     success, img = cap.read()
     if success:
         results = eye_tracker(img)
-        for i in range(len(results.boxes)):
-            box = results.boxes[i]
-            tensor = box.xyxy
-            values_list = tensor[0].tolist()
-            if len(values_list) != 0:
-                eye_boxes = {'x1': values_list[0], 'y1': values_list[1], 'x2': values_list[2], 'y2': values_list[3]}
-                pprint(eye_boxes)
-
-            #print(values_list)
-            #print("Probability:", box.conf[0].item())
+        pprint(results)
+        if len(results) != 0:
+            pupil_coordinates = (int((results['x1'] + results['x2'])/2), int((results['y1'] + results['y2'])/2))
+            cv2.circle(img, pupil_coordinates, 3, (0, 255, 0), cv2.FILLED)
 
     cv2.imshow("Eye Tracking", img)
     cv2.waitKey(1)
